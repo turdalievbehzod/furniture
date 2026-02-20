ae772503013445b9bcda8be838c9f09e6f139331
turdalievbehzod <turdalievbehzod58@gmail.com>
2026-02-20 17:50:28 +0500

    db created

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=32)
+    
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return f"{self.id} | {self.title}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=32)
+    image = models.ImageField(upload_to='products/')
+    info = models.CharField(max_length=128)
+        
+    category = models.ForeignKey(
+        "Category", on_delete=models.PROTECT)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'post'
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        
+    def __str__(self):
+        return f"{self.id} | {self.title}"
+    
+class Tag(models.Model):
+    title = models.CharField(max_length=128)
+    
+    class Meta:
+        db_table = 'tag'
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+        
--- shared/models.py
+++ shared/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='media')
+    description = models.CharField(max_length=255)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+updated_at = models.DateTimeField(auto_now=True)
