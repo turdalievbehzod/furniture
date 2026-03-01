3732b36416cf629e76b291ac049f3a5e564e970e
turdalievbehzod <turdalievbehzod58@gmail.com>
2026-03-01 22:36:30 +0500

    AI code replaced

--- users/urls.py
+++ users/urls.py
@@ -5,11 +5,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('login.html', login_view, name='login-html'),
     path('register/', register_view, name='register'),
-    path('register.html', register_view, name='register-html'),
     path('account/', account_view, name='account'),
-    path('account.html', account_view, name='account-html'),
-    path('reset-password/', reset_password_view, name='reset-password'),
-    path('reset-password.html', reset_password_view, name='reset-password-html'),
+    path('reset-password/', reset_password_view, name='reset-password'), 
 ]
